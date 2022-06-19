aef1711a4a3c39909ee4e339b31fe8848397988f
mbartlett21 <mjmouse9999@gmail.com>
2022-06-20 09:36:08 +1000

    Various updates: - Use the builtin acceleration - Don't set velocity directly (except to stop completely) - Slow down if there is no driver - Allow a single passenger

--- init.lua
+++ init.lua
@@ -1,23 +1,45 @@
+-- This is an edited version of Paramat's airboat mod:
+--  https://github.com/paramat/airboat
+
+local acceleration = 2
+
+local friction = 0.1
+
+local acceleration_y = 1
+
+local friction_y = 0.25
+
+local yaw_speed = 0.015
+
 -- Functions
 
-local function get_sign(i)
-    if i == 0 then
-        return 0
-    else
-        return i / math.abs(i)
+local function airboat_manage_attachment(player, obj, is_passenger)
+    if not player then
+        return
     end
-end
+    local status = obj ~= nil
+    local player_name = player:get_player_name()
+    if player_api.player_attached[player_name] == status then
+        return
+    end
+    player_api.player_attached[player_name] = status
 
-
-local function get_velocity(v, yaw, y)
-    local x = -math.sin(yaw) * v
-    local z =  math.cos(yaw) * v
-    return {x = x, y = y, z = z}
-end
-
-
-local function get_v(v)
-    return math.sqrt(v.x ^ 2 + v.z ^ 2)
+    if status then
+        local attach = player:get_attach()
+        if attach and attach:get_luaentity() then
+            local luaentity = attach:get_luaentity()
+            if luaentity.driver then
+                luaentity.driver = nil
+            end
+            player:set_detach()
+        end
+        player:set_attach(obj, "", {x = is_passenger and 5 or -5, y = -3, z = 0}, {x = 0, y = 0, z = 0})
+        player:set_eye_offset({x = 0, y = 6, z = 0}, {x = 0, y = 9, z = 0})
+    else
+        player:set_detach()
+        player:set_eye_offset({x = 0, y = 0, z = 0}, {x = 0, y = 0, z = 0})
+        player_api.set_animation(player, "stand" , 30)
+    end
 end
 
 
@@ -34,127 +56,152 @@ local airboat_entity = {
     },
 
     -- Custom fields
-    driver = nil,
-    removed = false,
-    v = 0,
-    vy = 0,
-    rot = 0,
-    auto = false,
+    driver = nil, -- Current driver
+    passenger = nil, -- Current passenger
+    auto = false, -- Cruise control
 }
 
 
-function airboat_entity.on_rightclick(self, clicker)
+function airboat_entity:on_rightclick(clicker)
     if not clicker or not clicker:is_player() then
         return
     end
     local name = clicker:get_player_name()
+
     if self.driver and name == self.driver then
-        -- Detach
         self.driver = nil
         self.auto = false
-        clicker:set_detach()
-        default.player_attached[name] = false
-        default.player_set_animation(clicker, "stand" , 30)
-        local pos = clicker:getpos()
-        minetest.after(0.1, function()
-            clicker:setpos(pos)
-        end)
+        -- Detach
+        airboat_manage_attachment(clicker, nil)
+
+    elseif self.passenger and name == self.passenger then
+        self.passenger = nil
+        -- Detach
+        airboat_manage_attachment(clicker, nil)
+
     elseif not self.driver then
-        -- Attach
-        local attach = clicker:get_attach()
-        if attach and attach:get_luaentity() then
-            local luaentity = attach:get_luaentity()
-            if luaentity.driver then
-                luaentity.driver = nil
-            end
-            clicker:set_detach()
-        end
         self.driver = name
-        clicker:set_attach(self.object, "",
-            {x = 0, y = -2, z = 0}, {x = 0, y = 0, z = 0})
-        default.player_attached[name] = true
+        airboat_manage_attachment(clicker, self.object)
+
+        -- player_api does not update the animation
+        -- when the player is attached, reset to default animation
         minetest.after(0.2, function()
-            default.player_set_animation(clicker, "sit" , 30)
+          player_api.set_animation(clicker, "sit" , 30)
+        end)
+        clicker:set_look_horizontal(self.object:getyaw())
+
+    elseif not self.passenger then
+        self.passenger = name
+        airboat_manage_attachment(clicker, self.object, true)
+
+        -- player_api does not update the animation
+        -- when the player is attached, reset to default animation
+        minetest.after(0.2, function()
+          player_api.set_animation(clicker, "sit" , 30)
         end)
         clicker:set_look_horizontal(self.object:getyaw())
     end
 end
 
-
-function airboat_entity.on_activate(self, staticdata, dtime_s)
+function airboat_entity:on_activate()
     self.object:set_armor_groups({immortal = 1})
 end
 
+function airboat_entity:on_detach_child(child)
+    if child and child:get_player_name() == self.driver then
+        airboat.cancel_go(self.driver)
+        self.driver = nil
+        self.auto = false
+    end
 
-function airboat_entity.on_punch(self, puncher)
-    if not puncher or not puncher:is_player() or self.removed then
+    if child and child:get_player_name() == self.passenger then
+        airboat.cancel_go(self.passenger)
+        self.passenger = nil
+    end
+end
+
+function airboat_entity:on_punch(puncher)
+    if not puncher or not puncher:is_player() then
         return
     end
 
-    local name = puncher:get_player_name()
-    if self.driver and name == self.driver then
-        -- Detach
-        self.driver = nil
-        puncher:set_detach()
-        default.player_attached[name] = false
-    end
-    if not self.driver then
+    -- Player can only pick it up if no-one is inside
+    if not self.driver and not self.passenger then
         -- Move to inventory
-        self.removed = true
-        local inv = puncher:get_inventory()
-        if not (creative and creative.is_enabled_for
-                and creative.is_enabled_for(name))
-                or not inv:contains_item("main", "airboat:airboat") then
-            local leftover = inv:add_item("main", "airboat:airboat")
-            if not leftover:is_empty() then
-                minetest.add_item(self.object:getpos(), leftover)
-            end
-        end
-        minetest.after(0.1, function()
+        creative.add_to_inventory(puncher, "airboat:airboat", self.object:get_pos())
+        minetest.after(0, function()
             self.object:remove()
         end)
     end
 end
 
 
-function airboat_entity.on_step(self, dtime)
-    self.v = get_v(self.object:getvelocity()) * get_sign(self.v)
-    self.vy = self.object:getvelocity().y
+
+function airboat_entity:on_step()
+    local object_v = self.object:get_velocity()
+
+    -- local horiz_vel_sq = object_v.x * object_v.x + object_v.y * object_v.y
+
+    local vx_acc = -object_v.x * friction
+    local vz_acc = -object_v.z * friction
+
+    local curr_yaw = self.object:getyaw()
+    local new_yaw = curr_yaw
+
+    local vx_acc_mult = -math.sin(curr_yaw)
+    local vz_acc_mult =  math.cos(curr_yaw)
+
+    local vy_acc = -object_v.y * friction_y
+
+    local horiz_stop = math.abs(object_v.x) < 0.1 and math.abs(object_v.z) < 0.1
 
     -- Controls
     if self.driver then
         local driver_objref = minetest.get_player_by_name(self.driver)
         if driver_objref then
             local ctrl = driver_objref:get_player_control()
+
+            -- Forward / backward
             if ctrl.up and ctrl.down then
+                vx_acc = vx_acc + acceleration * vx_acc_mult
+                vz_acc = vz_acc + acceleration * vz_acc_mult
                 if not self.auto then
                     self.auto = true
-                    minetest.chat_send_player(self.driver,
-                        "[airboat] Cruise on")
+                    minetest.chat_send_player(self.driver, "[airboat] Cruise on")
                 end
+                horiz_stop = false
             elseif ctrl.down then
-                self.v = self.v - 0.1
+                vx_acc = vx_acc - acceleration * vx_acc_mult
+                vz_acc = vz_acc - acceleration * vz_acc_mult
                 if self.auto then
                     self.auto = false
-                    minetest.chat_send_player(self.driver,
-                        "[airboat] Cruise off")
+                    minetest.chat_send_player(self.driver, "[airboat] Cruise off")
                 end
+                horiz_stop = false
             elseif ctrl.up or self.auto then
-                self.v = self.v + 0.1
+                vx_acc = vx_acc + acceleration * vx_acc_mult
+                vz_acc = vz_acc + acceleration * vz_acc_mult
+                horiz_stop = false
             end
-            if ctrl.left then
-                self.rot = self.rot + 0.001
+
+            if ctrl.left and ctrl.right then
+                -- Do nothing
+            elseif ctrl.left then
+                new_yaw = new_yaw + yaw_speed
             elseif ctrl.right then
-                self.rot = self.rot - 0.001
+                new_yaw = new_yaw - yaw_speed
             end
+
             if ctrl.jump then
-                self.vy = self.vy + 0.075
+                vy_acc = vy_acc + acceleration_y
+                horiz_stop = false
             elseif ctrl.sneak then
-                self.vy = self.vy - 0.075
+                vy_acc = vy_acc - acceleration_y
+                horiz_stop = false
             end
         else
             -- Player left server while driving
-            -- In MT 5.0.0 use 'airboat:on_detach_child()' to do this
+            airboat.cancel_go(self.driver)
             self.driver = nil
             self.auto = false
             minetest.log("warning", "[airboat] Driver left server while" ..
@@ -163,55 +210,32 @@ function airboat_entity.on_step(self, dtime)
         end
     end
 
-    -- Early return for stationary vehicle
-    if self.v == 0 and self.rot == 0 and self.vy == 0 then
-        self.object:setpos(self.object:getpos())
-        return
+    if horiz_stop then
+        vx_acc = 0
+        vz_acc = 0
+        self.object:set_velocity({ x = 0, y = object_v.y, z = 0 })
     end
 
-    -- Reduction and limiting of linear speed
-    local s = get_sign(self.v)
-    self.v = self.v - 0.02 * s
-    if s ~= get_sign(self.v) then
-        self.v = 0
-    end
-    if math.abs(self.v) > 6 then
-        self.v = 6 * get_sign(self.v)
+    if not self.driver then
+        vy_acc = vy_acc - 0.5
+        -- Slow down when there's no driver
+        vx_acc = vx_acc * 3
+        vz_acc = vz_acc * 3
     end
 
-    -- Reduction and limiting of rotation
-    local sr = get_sign(self.rot)
-    self.rot = self.rot - 0.0003 * sr
-    if sr ~= get_sign(self.rot) then
-        self.rot = 0
-    end
-    if math.abs(self.rot) > 0.015 then
-        self.rot = 0.015 * get_sign(self.rot)
-    end
-
-    -- Reduction and limiting of vertical speed
-    local sy = get_sign(self.vy)
-    self.vy = self.vy - 0.03 * sy
-    if sy ~= get_sign(self.vy) then
-        self.vy = 0
-    end
-    if math.abs(self.vy) > 4 then
-        self.vy = 4 * get_sign(self.vy)
-    end
-
-    local new_acce = {x = 0, y = 0, z = 0}
+    local new_acce = { x = vx_acc, y = vy_acc, z = vz_acc }
     -- Bouyancy in liquids
-    local p = self.object:getpos()
+    local p = self.object:get_pos()
     p.y = p.y - 2
     local def = minetest.registered_nodes[minetest.get_node(p).name]
     if def and (def.liquidtype == "source" or def.liquidtype == "flowing") then
-        new_acce = {x = 0, y = 10, z = 0}
+        new_acce.y = new_acce.y + 10
     end
 
-    self.object:setpos(self.object:getpos())
-    self.object:setvelocity(get_velocity(self.v, self.object:getyaw(), self.vy))
-    self.object:setacceleration(new_acce)
-    self.object:setyaw(self.object:getyaw() + (1 + dtime) * self.rot)
+    self.object:set_acceleration(new_acce)
+    if new_yaw ~= curr_yaw then
+        self.object:set_yaw(new_yaw)
+    end
 end
 
 
@@ -221,69 +245,70 @@ minetest.register_entity("airboat:airboat", airboat_entity)
 -- Craftitem
 
 minetest.register_craftitem("airboat:airboat", {
-    description = "Airboat",
-    inventory_image = "airboat_airboat_inv.png",
-    wield_scale = {x = 2, y = 2, z = 2},
-    liquids_pointable = true,
+        description = "Airboat",
+        inventory_image = "airboat_airboat_inv.png",
+        wield_scale = {x = 2, y = 2, z = 2},
+        liquids_pointable = true,
 
-    on_place = function(itemstack, placer, pointed_thing)
-        local under = pointed_thing.under
-        local node = minetest.get_node(under)
-        local udef = minetest.registered_nodes[node.name]
+        on_place = function(itemstack, placer, pointed_thing)
+            local under = pointed_thing.under
+            local node = minetest.get_node(under)
+            local udef = minetest.registered_nodes[node.name]
 
-        -- Run any on_rightclick function of pointed node instead
-        if udef and udef.on_rightclick and
-                not (placer and placer:is_player() and
-                placer:get_player_control().sneak) then
-            return udef.on_rightclick(under, node, placer, itemstack,
-                pointed_thing) or itemstack
-        end
-
-        if pointed_thing.type ~= "node" then
-            return itemstack
-        end
-
-        pointed_thing.under.y = pointed_thing.under.y + 2.5
-        local airboat = minetest.add_entity(pointed_thing.under,
-            "airboat:airboat")
-        if airboat then
-            if placer then
-                airboat:setyaw(placer:get_look_horizontal())
+            -- Run any on_rightclick function of pointed node instead
+            if udef and udef.on_rightclick and
+                    not (placer and placer:is_player() and
+                    placer:get_player_control().sneak) then
+                return udef.on_rightclick(under, node, placer, itemstack,
+                    pointed_thing) or itemstack
             end
-            creative.take_from_inventory(placer, itemstack)
-        end
-        return itemstack
-    end,
-    stack_max = 1,
-    groups = { creative = 1 }
+
+            if pointed_thing.type ~= "node" then
+                return itemstack
+            end
+
+            pointed_thing.under.y = pointed_thing.under.y + 2.5
+            local airboat = minetest.add_entity(pointed_thing.under,
+                "airboat:airboat")
+            if airboat then
+                if placer then
+                    airboat:setyaw(placer:get_look_horizontal())
+                end
+                creative.take_from_inventory(placer, itemstack)
+            end
+            return itemstack
+        end,
+        stack_max = 1,
+        groups = { creative = 1 }
 })
 
 
 -- Nodebox for entity wielditem visual
 
 minetest.register_node("airboat:airboat_nodebox", {
-    description = "Airboat Nodebox",
-    tiles = { -- Top, base, right, left, front, back
-        "airboat_airboat_top.png",
-        "airboat_airboat_base.png",
-        "airboat_airboat_right.png",
-        "airboat_airboat_left.png",
-        "airboat_airboat_front.png",
-        "airboat_airboat_back.png",
-    },
-    paramtype = "light",
-    drawtype = "nodebox",
-    node_box = {
-        type = "fixed",
-        fixed = {
-            -- Wmin,  hmin,  lmin, wmax,  hmax,  lmax
-            { -5/16, -1/8,  -1/2,  5/16,  3/8,   1/2 }, -- Envelope
-            { -3/16, -1/2,  -1/4,  3/16, -1/8,   1/4 }, -- Gondola
-            { -1/32,  3/8,  -1/2,  1/32,  1/2,  -1/4 }, -- Top fin
-            { -1/32, -1/4,  -1/2,  1/32, -1/8,  -1/4 }, -- Base fin
-            { -7/16,  3/32, -1/2, -5/16,  5/32, -1/4 }, -- Left fin
-            {  5/16,  3/32, -1/2,  7/16,  5/32, -1/4 }, -- Right fin
+        description = "Airboat Nodebox",
+        tiles = { -- Top, base, right, left, front, back
+            "airboat_airboat_top.png",
+            "airboat_airboat_base.png",
+            "airboat_airboat_right.png",
+            "airboat_airboat_left.png",
+            "airboat_airboat_front.png",
+            "airboat_airboat_back.png",
         },
-    },
-    groups = {not_in_creative_inventory = 1},
+        paramtype = "light",
+        drawtype = "nodebox",
+        node_box = {
+            type = "fixed",
+            fixed = {
+                -- Wmin,  hmin,  lmin, wmax,  hmax,  lmax
+                { -5/16, -1/8,  -1/2,  5/16,  3/8,   1/2 }, -- Envelope
+                { -3/16, -1/2,  -1/4,  3/16, -1/8,   1/4 }, -- Gondola
+
+                { -1/32,  3/8,  -1/2,  1/32,  1/2,  -1/4 }, -- Top fin
+                { -1/32, -1/4,  -1/2,  1/32, -1/8,  -1/4 }, -- Base fin
+                { -7/16,  3/32, -1/2, -5/16,  5/32, -1/4 }, -- Left fin
+                {  5/16,  3/32, -1/2,  7/16,  5/32, -1/4 }, -- Right fin
+            },
+        },
+        groups = { not_in_creative_inventory = 1 },
 })
